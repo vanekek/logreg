54d92b6d161b0a92b4b603b33408c568e96f3ecb
vanekek <kolesnikov.ia@phystech.edu>
2023-09-30 23:18:41 +0300

    Create main functionality

---
+++ logreg/train_test.py
@@ -0,0 +1,39 @@
+import torch
+
+def train(model, criterion, optimizer, train_loader, num_epochs, input_size):
+    # Train the model
+    model.train()
+    total_step = len(train_loader)
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            # Reshape images to (batch_size, input_size)
+            images = images.reshape(-1, input_size)
+            
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            if (i+1) % 100 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+def test(model, test_loader, input_size):
+    # Test the model
+    # In test phase, we don't need to compute gradients (for memory efficiency)
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.reshape(-1, input_size)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum()
+
+        print('Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
