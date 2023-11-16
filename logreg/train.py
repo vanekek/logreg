86295261f417bf41e81bcdc1948745520ebb5865
vanekek <kolesnikov.ia@phystech.edu>
2023-11-16 20:44:14 +0300

    Add hydra

--- logreg/train.py
+++ logreg/train.py
@@ -1,32 +1,38 @@
+import hydra
 import torch
 import torch.nn as nn
 from mnist import load_train_data
 from model import LogReg
+from omegaconf import DictConfig
 from train_test import train
 
 
-def main(num_epochs=2, batch_size=100, learning_rate=1e-3, save_path="model.pth"):
+@hydra.main(config_path="conf", config_name="train_config", version_base="1.3")
+def main(cfg: DictConfig):
     # Device configuration
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Hyper-parameters
-    input_size = 784  # 28x28
-    num_classes = 10
-
     # Load data
-    train_loader = load_train_data(batch_size)
+    train_loader = load_train_data(cfg["training"]["batch_size"])
 
     # Initiating model
-    model = LogReg(input_size, num_classes).to(device)
+    model = LogReg(cfg["model"]).to(device)
 
     # Loss and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=cfg["training"]["learning_rate"])
 
     # Train the model
-    train(model, criterion, optimizer, train_loader, num_epochs, input_size)
+    train(
+        model,
+        criterion,
+        optimizer,
+        train_loader,
+        cfg["training"]["num_epochs"],
+        cfg["model"]["input_size"],
+    )
 
-    torch.save(model.state_dict(), save_path)
+    torch.save(model.state_dict(), cfg["training"]["save_path"])
 
 
 if __name__ == "__main__":
