b772dd3ff6f843af516db0e270cdac39a309f11b
vanekek <kolesnikov.ia@phystech.edu>
2023-11-17 15:15:32 +0300

    Add hydra config for infer.py

--- logreg/infer.py
+++ logreg/infer.py
@@ -1,22 +1,22 @@
 import torch
+import hydra
+from omegaconf import DictConfig, OmegaConf
 from mnist import load_test_data
 from model import LogReg
 from train_test import test
 
+@hydra.main(config_path="conf", config_name="test_config", version_base="1.3")
+def main(cfg: DictConfig):
 
-def main(batch_size=100, save_path="model.pth", pred_path="predictions.csv"):
-    # Hyper-parameters
-    input_size = 784  # 28x28
-    num_classes = 10
-
-    model = LogReg(input_size, num_classes)
-    model.load_state_dict(torch.load(save_path))
+    model_params = OmegaConf.to_container(cfg["model"])
+    model = LogReg(**model_params)
+    model.load_state_dict(torch.load(cfg["test"]["save_path"]))
     model.eval()
 
-    test_loader = load_test_data(batch_size)
+    test_loader = load_test_data(cfg["test"]["batch_size"])
 
     # Test the model
-    test(model, test_loader, input_size, pred_path)
+    test(model, test_loader, cfg["model"]["input_size"], cfg["test"]["pred_path"])
 
 
 if __name__ == "__main__":
